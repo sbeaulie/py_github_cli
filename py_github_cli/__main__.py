8d2de8144453326174b7c11855175fbcb2039bd6
sbeaulie <sbeaulie@users.noreply.github.com>
2022-08-14 00:53:42 +0000

    ✅ Ready to clone and code.

--- py_github_cli/__main__.py
+++ py_github_cli/__main__.py
@@ -1,4 +1,4 @@
-"""Entry point for project_name."""
+"""Entry point for py_github_cli."""
 
 from .cli import main  # pragma: no cover
 
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from py_github_cli.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "py_github_cli"
